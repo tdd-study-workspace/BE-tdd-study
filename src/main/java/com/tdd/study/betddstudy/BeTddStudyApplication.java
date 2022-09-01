077b64a03f64f96aa294743c4b0c0d2453600d7b
윤혁준 <gurwns1540@gmail.com>
2022-09-01 18:17:47 +0900

    init

---
+++ src/main/java/com/tdd/study/betddstudy/BeTddStudyApplication.java
@@ -0,0 +1,13 @@
+package com.tdd.study.betddstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeTddStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeTddStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tdd/study/betddstudy/BeTddStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tdd.study.betddstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeTddStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
