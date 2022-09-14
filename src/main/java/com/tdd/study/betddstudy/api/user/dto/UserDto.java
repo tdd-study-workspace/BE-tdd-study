d24d9aa74b23b35106d208f9e501e0035b2249f4
윤혁준 <gurwns1540@gmail.com>
2022-09-14 18:38:38 +0900

    RGB-32 변경사항   1. red test case 작성 중

---
+++ src/main/java/com/tdd/study/betddstudy/api/user/dto/UserDto.java
@@ -0,0 +1,17 @@
+package com.tdd.study.betddstudy.api.user.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@ToString
+public class UserDto {
+    private String email;
+    private String token;
+    private String username;
+    private String bio;
+    private String image;
+}
--- src/test/java/com/tdd/study/betddstudy/user/UserTests.java
+++ src/test/java/com/tdd/study/betddstudy/user/UserTests.java
@@ -1,11 +1,42 @@
 package com.tdd.study.betddstudy.user;
 
+import com.tdd.study.betddstudy.api.user.entity.User;
+import com.tdd.study.betddstudy.api.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureTestEntityManager;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.*;
 
 @AutoConfigureTestEntityManager
 @AutoConfigureTestDatabase
 @SpringBootTest
 public class UserTests {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    @DisplayName("최근 사용자 조회하는 기능 테스트")
+    void getCurrentUserTest() {
+        //given
+        User user = User.builder()
+                .id(0L)
+                .name("test-user")
+                .password("test-password")
+                .build();
+
+        userRepository.save(user);
+
+        //when
+        User currentUser = userRepository.findById(0L).orElseThrow();
+
+
+        //then
+
+    }
 }
